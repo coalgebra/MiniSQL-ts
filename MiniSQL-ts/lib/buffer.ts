03230425639387a445d8e9adbb8836e4e579fcca
coalgebra <35209135+coalgebra@users.noreply.github.com>
2018-06-27 19:27:57 +0800

    Update buffer.ts

--- MiniSQL-ts/lib/buffer.ts
+++ MiniSQL-ts/lib/buffer.ts
@@ -1,3 +1,21 @@
-﻿export class BufferManager {
+export interface IBufferMananger {
+    createFile(filename: string);
+    destroyFile(filename: string);
+    openFile(filename: string): IFileHandle;
+    closeFile(filename: string);
+}
+
+export interface IFileHandle {
+    getFirstBlock(): Block;
+    getLastBlock(): Block;
+    getNextBlock(blockId: number): Block;
+    getPrevBlock(blockId: number): Block;
+    pinBlock();
+    unpinBlock();
+    setDirty();
+    isDirty(): bool;
+}
+
+export class BufferManager {
     
 }
